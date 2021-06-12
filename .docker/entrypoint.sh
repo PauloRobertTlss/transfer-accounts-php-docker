b51a1c7a691e4e7472a327387582ac31e005f29e
psilva <psilva@bionexo.com>
2021-06-12 15:24:02 -0300

    [docker] started devops docker

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd src
+
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
+if [ ! -f ".env.testing" ]; then
+  cp .env.testing.example .env.testing
+fi
+
+composer install
+php artisan key:generate
+php artisan migrate
+
+php-fpm
---
+++ .docker/mysql/initdb.sql
@@ -0,0 +1,2 @@
+    CREATE DATABASE IF NOT EXISTS transfer_accounts;
+    CREATE DATABASE IF NOT EXISTS transfer_accounts_tests;
