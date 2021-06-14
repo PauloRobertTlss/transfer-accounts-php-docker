0c596c55761888a9be94630b4d81f3f2eaeb46e1
psilva <psilva@bionexo.com>
2021-06-14 20:39:36 -0300

    [resources] to transactions | router

--- src/routes/api.php
+++ src/routes/api.php
@@ -12,7 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function () {
+    Route::post('transfers', 'TransactionsController@create')->name('transfer.create');
+    Route::get('clients', 'ClientsController@index')->name('clients.all');
+    Route::get('clients/persons', 'ClientsController@persons')->name('clients.persons');
+    Route::get('clients/shopkeepers', 'ClientsController@shopkeepers')->name('clients.shopkeepers');
 });
