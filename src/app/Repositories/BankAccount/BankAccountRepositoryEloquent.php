9d4e08d3193236bf86f151672375f4e533b6af78
psilva <psilva@bionexo.com>
2021-06-16 22:36:07 -0300

    [repository] started operations per category

--- src/app/Repositories/BankAccount/BankAccountRepositoryEloquent.php
+++ src/app/Repositories/BankAccount/BankAccountRepositoryEloquent.php
@@ -3,7 +3,12 @@
 namespace App\Repositories\BankAccount;
 
 use App\Common\Repositories\BaseRepository;
+use App\Domain\Financial\BankAccount\Entity\Contract\BankAccountInterface;
 use App\Domain\Financial\BankAccount\Repository\BankAccountRepositoryInterface;
+use App\Domain\Financial\Transaction\Entity\Contract\{CategoryPayeeInterface, CategoryPayerInterface};
+use App\Domain\Financial\Transaction\Repository\{TransactionPayeeRepositoryInterface,
+    TransactionPayerRepositoryInterface
+};
 use App\Models\Financial\BankAccount\BankAccountModel;
 
 /**
@@ -12,16 +17,47 @@ use App\Models\Financial\BankAccount\BankAccountModel;
  */
 class BankAccountRepositoryEloquent extends BaseRepository implements BankAccountRepositoryInterface
 {
+    /**
+     * @var TransactionPayerRepositoryInterface
+     */
+    private TransactionPayerRepositoryInterface $payerRepository;
+    /**
+     * @var TransactionPayeeRepositoryInterface
+     */
+    private TransactionPayeeRepositoryInterface $payeeRepository;
+
+    /**
+     * BankAccountRepositoryEloquent constructor.
+     * @param TransactionPayerRepositoryInterface $payerRepository
+     * @param TransactionPayeeRepositoryInterface $payeeRepository
+     */
+    public function __construct(TransactionPayerRepositoryInterface $payerRepository,
+                                TransactionPayeeRepositoryInterface $payeeRepository)
+    {
+        parent::__construct();
+
+        $this->payerRepository = $payerRepository;
+        $this->payeeRepository = $payeeRepository;
+    }
+
     public function model(): string
     {
         return BankAccountModel::class;
     }
 
-    public function addBalance(int $id,float $newValue)
+    public function addBalance(int $id, float $newValue, CategoryPayeeInterface $category)
     {
-        $model = $this->scope->find($id);
-        $model->balance = $model->balance + $newValue;
+        /** @var BankAccountInterface $model */
+        $model = $this->scope->with('client')->find($id);
+        $model->balance = $category->operation($model->balance, $newValue);
         $model->save();
+
+        $this->payeeRepository->create([
+            'value' => $newValue,
+            'category' => $category->identifier(),
+            'bank_account_id' => $model->id(),
+            'client_done_id' => $model->getClient()->uuid()
+        ]);
 
         // broadcast(new BankAccountBalanceUpdatedEvent($model)); //real-time para o Pusher
         return $model;
@@ -29,11 +65,19 @@ class BankAccountRepositoryEloquent extends BaseRepository implements BankAccoun
     }
 
 
-    public function decreaseBalance(int $id, float $newValue)
+    public function decreaseBalance(int $id, float $newValue, CategoryPayerInterface $category)
     {
-        $model = $this->scope->find($id);
-        $model->balance = $model->balance - $newValue;
+        /** @var BankAccountInterface $model */
+        $model = $this->scope->with('client')->find($id);
+        $model->balance = $category->operation($model->balance, $newValue);
         $model->save();
+
+        $this->payerRepository->create([
+            'value' => $newValue,
+            'category' => $category->identifier(),
+            'bank_account_id' => $model->id(),
+            'client_done_id' => $model->getClient()->uuid()
+        ]);
 
         // broadcast(new BankAccountBalanceUpdatedEvent($model)); //real-time para o Pusher
         return $model;
