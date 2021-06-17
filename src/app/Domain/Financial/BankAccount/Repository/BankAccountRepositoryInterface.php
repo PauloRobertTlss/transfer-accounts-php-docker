167d8f4ebac9c26488b37019c2024c0b303fa2c5
psilva <psilva@bionexo.com>
2021-06-16 22:33:15 -0300

    [domain] interface operations category

--- src/app/Domain/Financial/BankAccount/Repository/BankAccountRepositoryInterface.php
+++ src/app/Domain/Financial/BankAccount/Repository/BankAccountRepositoryInterface.php
@@ -2,11 +2,28 @@
 
 namespace App\Domain\Financial\BankAccount\Repository;
 
+use App\Domain\Financial\Transaction\Entity\Contract\{CategoryPayeeInterface,CategoryPayerInterface};
+
+/**
+ * Interface BankAccountRepositoryInterface
+ * @package App\Domain\Financial\BankAccount\Repository
+ */
 interface BankAccountRepositoryInterface
 {
+    /**
+     * @param int $id
+     * @param float $value
+     * @param CategoryPayeeInterface $category
+     * @return mixed
+     */
+    public function addBalance(int $id, float $value, CategoryPayeeInterface $category);
 
-    public function addBalance(int $id, float $value);
-
-    public function decreaseBalance(int $id, float $value);
+    /**
+     * @param int $id
+     * @param float $value
+     * @param CategoryPayerInterface $category
+     * @return mixed
+     */
+    public function decreaseBalance(int $id, float $value, CategoryPayerInterface $category);
 
 }
