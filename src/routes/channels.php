240e7d358285aa1b4cde8c9e6f4353dcabdda1ff
psilva <psilva@bionexo.com>
2021-06-23 23:48:30 -0300

    [insinghts] analysis average of 1.15 cyclomatic complexity

--- src/routes/channels.php
+++ src/routes/channels.php
@@ -1,15 +1,4 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+<?php declare(strict_types=1);
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
